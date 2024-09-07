6665c2fe11308f9290841def71cb395bfccef589
franciscobenavides876 <134728916+franciscobenavides876@users.noreply.github.com>
2024-09-07 14:42:13 -0400

    Actualizacion en el Directorio
    Actualización completa en el directorio

---
+++ Frontend - Francisco Benavides/src/Paginas/ReservaEspecial.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ReservaEspecial = () =>{
+    return <div>
+        Reserva Especial
+    </div>
+}
+
+export default ReservaEspecial
---
+++ Frontend - Francisco Benavides/src/Paginas/ReservayEquipo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ReservayEquipo = () =>{
+    return <div>
+        Reserva y Equipo
+    </div>
+}
+
+export default ReservayEquipo
