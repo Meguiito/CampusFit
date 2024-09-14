9936d408c433c45b5effc48f66878f12f542534e
VicenteAZB <135163471+VicenteAZB@users.noreply.github.com>
2024-09-14 00:02:47 -0300

    Delete app/APIs Login & Register directory

--- app/APIs Login & Register/conex.py
+++
@@ -1,4 +0,0 @@
-from pymongo import MongoClient
-cliente = MongoClient("mongodb+srv://vicentealvarez2023:1000Hppower.@campusfit.xih68.mongodb.net/")
-db = cliente["CampusFIT_DB"]
-coleccion = db["Usuarios"]
