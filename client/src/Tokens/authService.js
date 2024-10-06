c3aace89a308718f01dae4bcd053fa284fc97704
VicenteAZB <vicente.alvarez2023@alu.uct.cl>
2024-10-06 18:06:11 -0300

    Mod

--- client/src/Tokens/authService.js
+++ client/src/Tokens/authService.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-// Interceptor para agregar el token de autorización en cada solicitud
 axios.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token');
@@ -14,33 +13,23 @@ axios.interceptors.request.use(
   }
 );
 
-// Función para iniciar sesión
 export const login = async (email, password) => {
   try {
-    // Realiza la solicitud de verificación del usuario
     const response = await axios.post('http://localhost:5000/users/verify', { email, password });
-    
-    // Almacena el token de acceso en el localStorage
-    localStorage.setItem('token', response.data.access_token); // Asegúrate de usar access_token
-    
-    return response.data; // Devuelve los datos del usuario
+    localStorage.setItem('token', response.data.access_token);
+    return response.data;
   } catch (error) {
-    // Manejo de errores
-    console.error('Error al iniciar sesión', error.response ? error.response.data : error.message);
-    throw error; // Lanza el error para manejarlo en el componente
+    console.error('Error al iniciar sesión', error);
+    throw error;
   }
 };
 
-// Función para cerrar sesión
 export const logout = () => {
-  // Elimina el token del localStorage
   localStorage.removeItem('token');
-  
-  // Redirige al usuario a la página de inicio de sesión
   window.location.href = '/Login';
 };
 
-// Función para obtener el token
 export const getToken = () => {
-  return localStorage.getItem('token'); // Devuelve el token almacenado
+  return localStorage.getItem('token');
 };
+
